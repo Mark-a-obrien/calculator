78c458aa1b0dac16dfedcb17b7d533b1e5a20330
Mark-a-obrien <mark.a.obrien@gmail.com>
2022-01-25 11:06:04 +0300

    created a calculator display and allowed for basic input

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+const display = document.querySelector(".display");
+let displayValue = "";
+
 function add(a, b) {
     return a+b;
 }
@@ -19,3 +22,21 @@ function divide(a, b) {
 function operator(a, b) {
     multiply(a, b);
 }
+
+
+
+
+// Numbers
+const buttons = document.querySelectorAll(".numbers");
+buttons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+        displayValue += button.textContent;
+        display.textContent = displayValue;
+        console.log(displayValue)
+        display.setAttribute("style", `background-color: "red";`);
+    })
+})
+
+
+const operators = document.querySelectorAll(".operators");
